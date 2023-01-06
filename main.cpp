c4874457a581e1d8401049385662e5911e2d5814
Raphael-Nyphalem <Nyphalem@pm.me>
2023-01-06 09:33:55 +0100

    def fonction 1

--- main.cpp
+++ main.cpp
@@ -1,4 +1,46 @@
 
+
+
+const int MAX_AFFICHE_X = 8;
+const int MAX_AFFICHE_Y = 8;
+
+
+
+//F1
+void moteurDroit()
+{
+    //gere les impulsions de commande du moteur Gauche
+
+}
+
+void moteurGauche()
+{
+    //gere les impulsions de commande du moteur Gauche
+}
+
+//F2
+
+
+
+//F3
+
+//F4
+void affichagePuissance()
+{
+    //
+}
+
+//F5
+
+//F6
+
+//F7
+
+
+
+
+
+
 int main(int argc, char const *argv[])
 {
     /* code */
