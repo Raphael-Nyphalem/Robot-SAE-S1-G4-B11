d35d7025361f9cf70d28e799e6eec9f2edf94b74
Raphael-Nyphalem <Nyphalem@pm.me>
2023-01-09 19:17:34 +0100

    ++

--- main.cpp
+++ main.cpp
@@ -18,6 +18,7 @@
 #include <iomanip>
 #include <chrono>
 #include <thread>
+#include <sensehat.h>
 using namespace std;
 using namespace std::this_thread; // sleep_for, sleep_until
 using namespace std::chrono; // system_clock, seconds, milliseconds
@@ -38,7 +39,9 @@ void gestion_mot_Droit(int puissance)
 {
     /*
     gere les impulsions de commande du moteur Droit 
-    en fonction d'une puissance en %
+    en fonction d'une puissance en % (0 a 100)
+    utilise
+        gpio raspberry
     */
 }
 
@@ -46,7 +49,8 @@ void gestion_mot_Gauche(int puissance)
 {
     /*
     gere les impulsions de commande du moteur Gauche 
-    en fonction d'une puissance en %
+    en fonction d'une puissance en % (0 a 100)
+        gpio raspberry
     */
 }
 
@@ -73,39 +77,65 @@ void avance_vit(int vit)
     utilise
         - avance_Vitesse_Droit
         - avance_Vitesse_Gauche
+        - detect_angle
+        - correction_angle
     */   
 }
 
 void stop_Mot_Gauche()
 {
-    //
+    /*
+    arret du moteur Gauche
+    utilise 
+        - gestion_mot_Gauche
+    */
 }
 
 void stop_Mot_Droit()
 {
-    //
+    /*
+    arret du moteur Gauche
+    utilise 
+        - gestion_mot_Gauche
+    */
 }
 
 
 //F2 Détection des capteurs
 bool detec_Capt_Droit()
 {
+    /*
+    Renvois Vrais si le capteur Droit detecte une bande noir
+    utilise
+        gpio raspberry
+    */
     bool detect;
-    //Renvois Vrais si le capteur Droit detecte une bande noir
-    return detect;//bool
+    //code
+    return detect;
 }
 
 bool detec_Capt_Gauche()
 {
-    //Renvois Vrais si le capteur Gauche detecte une bande noir
+    /*
+    Renvois Vrais si le capteur Gauche detecte une bande noir
+    utilise
+        gpio raspberry
+    */
     bool detect;
+    //code
     return detect;
 }
 
 bool detec_2_Capt()
 {
-    //Renvois Vrais si les deuc capteurs detecte une bande noir
+    /*
+    Renvois Vrais si les 2 capteurs detecte une bande noir
+    utilise
+        - detec_Capt_Gauche
+        - detec_Capt_Droit
+    */
     bool detect;
+    //code
     return detect;
 }
 
@@ -119,6 +149,7 @@ void avance_dis(int dis,int vit)
         - detect_temp
         - stop
     */
+
 }
 void avance_valon(int dis,int vit)
 {
@@ -177,48 +208,71 @@ void stop()
 
 
 //F4 Gestion d'affichage 
-// ATENTION les input ne sont pas encore fais
+// ATTENTION les inputs ne sont pas encore fais
 void affich_Puissance_mot()
 {
     /*
     Affiche la puissance des moteurs gauche et droite sur l'afficheur LED (en vertical)
-     ainsi que la répartition de puissance entre les deux moteurs (horizontal)
+    utilise
+        <sensehat.h>
     */
 }
 
 void affich_repart_puissance()
 {
-    //
+    /*
+    Affiche la répartition de puissance entre les deux moteurs (horizontalement)
+    utilise
+        <sensehat.h>
+    */
 }
 
 void affich_perdu()
 {
-    //
+    /*
+    Affiche le message si le robot est perdu
+    utilise
+        <sensehat.h>
+    */
 }
 
 void affich_chargement()
 {
-    //
+    /*
+    Affiche l'object qui est charger
+    utilise
+        <sensehat.h>
+    */
 }
 
 void affich_direction()
 {
-    //
+    /*
+    Affiche le prochain itineraire du robot
+    utilise
+        <sensehat.h>
+    */
 }
 
 
 //F5 Gestion du Joystick
 bool detect_HAUT()
 {
-    //
+    /*
+    detect si on met le joystick vers le haut
+    */
     bool detect;
+    //code
     return detect;
 }
 
 bool detect_BAS()
 {
-    //
+    /*
+    detect si on met le joystick vers le bas
+    */
     bool detect;
+    //code
     return detect;
 }
 
@@ -226,6 +280,7 @@ bool detect_Droite()
 {
     //  
     bool detect;
+    //code
     return detect;
 }
 
@@ -233,6 +288,7 @@ bool detect_Gauche()
 {
     //  
     bool detect;
+    //code
     return detect; 
 }
 
@@ -240,6 +296,7 @@ bool detect_Bouton()
 {
     //   
     bool detect;
+    //code
     return detect; 
 }
 
@@ -247,12 +304,14 @@ bool detect_Bouton()
 char clavier()
 {
     char clv;
+    //code
     return clv;
 }
 
 bool detect_clavier(size_t touche)
 {
     bool detect;
+    //code
     return detect;
 }
 
@@ -260,15 +319,25 @@ bool detect_clavier(size_t touche)
 bool detect_temp(int time_x, int time_init)
 {
     bool detect;
+    //code
     return detect;
 }
 
 //F8 gestion Boussole
+int get_angle()
+{
+    int angle;
+    //code
+    return angle;
+}
+
 bool detect_angle(int angle)
 {
     bool detect;
+    //code
     return detect;
 }
+
 
 //F9 Autre
 void perdu()
@@ -280,6 +349,7 @@ int temps_1m()
 {
     //
     int time;
+    //code
     return time;
 }
 
@@ -288,41 +358,104 @@ void secur(int timemax)
 
 }
 
+void correction_angle(int angle,int vit)
+{
+    /*
+    corrige l'angle en fonction a quelle point il est deporte de sa direction
+    utilise
+        - avance_Vitesse_Droit
+        - avance_Vitesse_Gauche
+        - get_angle
+        - detect_angle
+    */
+}
+
+
+
 //F10 Trajet
 
 void base_chargement()
 {
-    //
+    /*
+    Décrit le chemin de la base du robot pour recharger sa batterie a la zone de chargement de la marchandise
+    utilise 
+        - avance_valon
+        - tourne_valon
+        - affich_direction
+        - stop
+    */
 }
 
 void chargement_A()
 {
-    //
+    /*
+    Décrit le chemin de la zone de chargement de la marchandise au point A
+    utilise 
+        - avance_valon
+        - tourne_valon
+        - affich_direction
+        - stop
+    */
 }
 
 void chargement_B()
 {
-    //
+    /*
+    Décrit le chemin de la zone de chargement de la marchandise au point B
+    utilise 
+        - avance_valon
+        - tourne_valon
+        - affich_direction
+        - stop
+    */
 }
 
 void A_chargement()
 {
-    //
+    /*
+    Décrit le chemin du point A j'usqu'a la zone de chargement de la marchandise 
+    utilise 
+        - avance_valon
+        - tourne_valon
+        - affich_direction
+        - stop
+    */
 }
 
 void B_chargement()
 {
-    //
+    /*
+    Décrit le chemin du point B j'usqu'a la zone de chargement de la marchandise 
+    utilise 
+        - avance_valon
+        - tourne_valon
+        - affich_direction
+        - stop
+    */
 }
 
 void A_base()
 {
-    //
+    /*
+    Décrit le chemin du point A j'usqu'a la base 
+    utilise 
+        - avance_valon
+        - tourne_valon
+        - affich_direction
+        - stop
+    */
 }
 
 void B_base()
 {
-    //
+    /*
+    Décrit le chemin du point B j'usqu'a la base 
+    utilise 
+        - avance_valon
+        - tourne_valon
+        - affich_direction
+        - stop
+    */
 }
 
 //SCENARIOS
