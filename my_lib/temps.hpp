f4bd3eea80716e15d4be638a085c25bfc59cc010
Raphael-Nyphalem <Nyphalem@pm.me>
2023-01-20 13:37:50 +0100

    lib et test temps

---
+++ my_lib/temps.hpp
@@ -0,0 +1,14 @@
+//temps.hpp
+#ifndef SAES1_TEMPS_HPP
+#define SAES1_TEMPS_HPP
+
+#include <iostream>
+#include <ctime>
+#include <chrono>
+
+namespace saeS1{
+
+
+} //namespace saeS1
+
+#endif //TEMPS.HPP
