2ab672c661ecfe07c2f726ec8538a13f38961d74
Raphael-Nyphalem <Nyphalem@pm.me>
2023-01-24 16:08:07 +0100

    trajet

--- my_lib/traject.cpp
+++ my_lib/traject.cpp
@@ -7,6 +7,7 @@ namespace saeS1{
         /*
         Décrit le chemin de la base du robot pour recharger sa batterie a la zone de chargement de la marchandise
         */
+       //double BC[MAX_FONCTIONS][MAX_PARAMETRE]= 
     }
 
     void chargement_A(double trajet[MAX_FONCTIONS][MAX_PARAMETRE])
@@ -14,6 +15,7 @@ namespace saeS1{
         /*
         Décrit le chemin de la zone de chargement de la marchandise au point A
         */
+       //double BC[MAX_FONCTIONS][MAX_PARAMETRE]= 
     }
 
     void chargement_B(double trajet[MAX_FONCTIONS][MAX_PARAMETRE])
@@ -21,6 +23,7 @@ namespace saeS1{
         /*
         Décrit le chemin de la zone de chargement de la marchandise au point B
         */
+       //double BC[MAX_FONCTIONS][MAX_PARAMETRE]= 
     }
 
     void A_chargement(double trajet[MAX_FONCTIONS][MAX_PARAMETRE])
@@ -28,6 +31,7 @@ namespace saeS1{
         /*
         Décrit le chemin du point A j'usqu'a la zone de chargement de la marchandise 
         */
+       //double BC[MAX_FONCTIONS][MAX_PARAMETRE]= 
     }
 
     void B_chargement(double trajet[MAX_FONCTIONS][MAX_PARAMETRE])
@@ -35,6 +39,7 @@ namespace saeS1{
         /*
         Décrit le chemin du point B j'usqu'a la zone de chargement de la marchandise 
         */
+       //double BC[MAX_FONCTIONS][MAX_PARAMETRE]= 
     }
 
     void A_base(double trajet[MAX_FONCTIONS][MAX_PARAMETRE])
@@ -42,6 +47,7 @@ namespace saeS1{
         /*
         Décrit le chemin du point A j'usqu'a la base 
         */
+       //double BC[MAX_FONCTIONS][MAX_PARAMETRE]= 
     }
 
     void B_base(double trajet[MAX_FONCTIONS][MAX_PARAMETRE])
@@ -49,6 +55,7 @@ namespace saeS1{
         /*
         Décrit le chemin du point B j'usqu'a la base 
         */
+       //double BC[MAX_FONCTIONS][MAX_PARAMETRE]= 
     }
 
 }//namespace
