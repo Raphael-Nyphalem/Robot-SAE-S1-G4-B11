e300d1d4b1bca8a55d864f2cd7d2f34553fa2280
Raphael-Nyphalem <Nyphalem@pm.me>
2023-01-25 11:35:43 +0100

    trajet

--- my_lib/traject.cpp
+++ my_lib/traject.cpp
@@ -18,8 +18,12 @@ namespace saeS1{
         /*
         Décrit le chemin de la base du robot pour recharger sa batterie a la zone de chargement de la marchandise
         */
-       //double BC[MAX_FONCTIONS][MAX_PARAMETRE]= 
-       //copieTab(trajet,BC);
+       double BC[MAX_FONCTIONS][MAX_PARAMETRE]= {
+        {0,1,0,0,0},{0,0,0,0,0},{0,0,0,0,0},{0,0,0,0,0},{0,0,0,0,0},
+       {0,0,0,0,0},{0,0,0,0,0},{0,0,0,0,0},{0,0,0,0,0},{0,0,0,0,0},
+       {0,0,0,0,0},{0,0,0,0,0},{0,0,0,0,0},{0,0,0,0,0},{0,0,0,0,0}};
+
+       copieTab(trajet,BC);
 
     }
     void chargement_base(double trajet[MAX_FONCTIONS][MAX_PARAMETRE])
@@ -27,8 +31,11 @@ namespace saeS1{
         /*
         Décrit le chemin de la la zone de chargement de la marchandise vers base du robot pour recharger sa batterie
         */
-       //double CB[MAX_FONCTIONS][MAX_PARAMETRE]= 
-       //copieTab(trajet,CB);
+       double CB[MAX_FONCTIONS][MAX_PARAMETRE]= {
+        {0,1,0,0,0},{0,0,0,0,0},{0,0,0,0,0},{0,0,0,0,0},{0,0,0,0,0},
+       {0,0,0,0,0},{0,0,0,0,0},{0,0,0,0,0},{0,0,0,0,0},{0,0,0,0,0},
+       {0,0,0,0,0},{0,0,0,0,0},{0,0,0,0,0},{0,0,0,0,0},{0,0,0,0,0}};
+       copieTab(trajet,CB);
     }
 
     void chargement_A(double trajet[MAX_FONCTIONS][MAX_PARAMETRE])
@@ -36,8 +43,11 @@ namespace saeS1{
         /*
         Décrit le chemin de la zone de chargement de la marchandise au point A
         */
-       //double CA[MAX_FONCTIONS][MAX_PARAMETRE]= 
-       //copieTab(trajet,CA);
+       double CA[MAX_FONCTIONS][MAX_PARAMETRE]= {
+        {0,1,0,0,0},{0,0,0,0,0},{0,0,0,0,0},{0,0,0,0,0},{0,0,0,0,0},
+       {0,0,0,0,0},{0,0,0,0,0},{0,0,0,0,0},{0,0,0,0,0},{0,0,0,0,0},
+       {0,0,0,0,0},{0,0,0,0,0},{0,0,0,0,0},{0,0,0,0,0},{0,0,0,0,0}};
+       copieTab(trajet,CA);
     }
 
     void chargement_B(double trajet[MAX_FONCTIONS][MAX_PARAMETRE])
@@ -45,8 +55,11 @@ namespace saeS1{
         /*
         Décrit le chemin de la zone de chargement de la marchandise au point B
         */
-       //double CB[MAX_FONCTIONS][MAX_PARAMETRE]= 
-       //copieTab(trajet,CB);
+       double CB[MAX_FONCTIONS][MAX_PARAMETRE]= {
+        {0,1,0,0,0},{0,0,0,0,0},{0,0,0,0,0},{0,0,0,0,0},{0,0,0,0,0},
+       {0,0,0,0,0},{0,0,0,0,0},{0,0,0,0,0},{0,0,0,0,0},{0,0,0,0,0},
+       {0,0,0,0,0},{0,0,0,0,0},{0,0,0,0,0},{0,0,0,0,0},{0,0,0,0,0}};
+       copieTab(trajet,CB);
     }
 
     void A_chargement(double trajet[MAX_FONCTIONS][MAX_PARAMETRE])
@@ -54,8 +67,11 @@ namespace saeS1{
         /*
         Décrit le chemin du point A j'usqu'a la zone de chargement de la marchandise 
         */
-       //double AC[MAX_FONCTIONS][MAX_PARAMETRE]= 
-       //copieTab(trajet,AC);
+       double AC[MAX_FONCTIONS][MAX_PARAMETRE]= {
+        {0,1,0,0,0},{0,0,0,0,0},{0,0,0,0,0},{0,0,0,0,0},{0,0,0,0,0},
+       {0,0,0,0,0},{0,0,0,0,0},{0,0,0,0,0},{0,0,0,0,0},{0,0,0,0,0},
+       {0,0,0,0,0},{0,0,0,0,0},{0,0,0,0,0},{0,0,0,0,0},{0,0,0,0,0}};
+       copieTab(trajet,AC);
     }
 
     void B_chargement(double trajet[MAX_FONCTIONS][MAX_PARAMETRE])
@@ -63,8 +79,11 @@ namespace saeS1{
         /*
         Décrit le chemin du point B j'usqu'a la zone de chargement de la marchandise 
         */
-       //double BC[MAX_FONCTIONS][MAX_PARAMETRE]= 
-       //copieTab(trajet,BC);
+       double BC[MAX_FONCTIONS][MAX_PARAMETRE]= {
+        {0,1,0,0,0},{0,0,0,0,0},{0,0,0,0,0},{0,0,0,0,0},{0,0,0,0,0},
+       {0,0,0,0,0},{0,0,0,0,0},{0,0,0,0,0},{0,0,0,0,0},{0,0,0,0,0},
+       {0,0,0,0,0},{0,0,0,0,0},{0,0,0,0,0},{0,0,0,0,0},{0,0,0,0,0}};
+       copieTab(trajet,BC);
     }
 
     /*
