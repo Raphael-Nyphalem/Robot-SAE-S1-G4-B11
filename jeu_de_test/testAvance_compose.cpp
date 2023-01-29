7c24fdef31f270f6246a7ee5b3167b84e41658f4
Raphael-Nyphalem <Nyphalem@pm.me>
2023-01-29 12:28:03 +0100

    test corerectcap

--- jeu_de_test/testAvance_compose.cpp
+++ jeu_de_test/testAvance_compose.cpp
@@ -24,6 +24,53 @@ using namespace saeS1;
 
 
 //TEST
+void test_correcton_cap()
+{
+    //Permet au robot d'avancer en ligne droite (simple) durant 1s
+    cout << " test_correcton_cap " <<endl;
+    double cap;
+    temps_t tps0;
+
+    get_temps(tps0);
+    //cas inverse (cap proche de 0)
+    cap = 1;
+    cout << "Debut cas inverse (cap proche de 0) " <<endl;
+    do
+    {
+        correction_angle(cap);
+    } while (!(detect_temps(10,tps0)));
+    cout << "Fin cas inverse (cap proche de 0) " <<endl;
+    
+    sleep_for(seconds(5));
+
+    //cas non-inverse
+    //cas entre 0 et 180,
+    cap = 90;
+    get_temps(tps0);
+    cout << "Debut cas non-inverse cap entre 0 et 180 " <<endl;
+    do
+    {
+        correction_angle(cap);
+    } while (!(detect_temps(10,tps0)));
+    cout << "Fin cas non-inverse cap entre 0 et 180 " <<endl;
+
+    sleep_for(seconds(5));
+
+    //cas entre 180 et 360,
+    cap = 270;
+    get_temps(tps0);
+    cout << "Debut cas non-inverse cap entre 180 et 360 " <<endl;
+    do
+    {
+        correction_angle(cap);
+    } while (!(detect_temps(10,tps0)));
+    cout << "Fin cas non-inverse cap entre 180 et 360 " <<endl;
+
+    
+
+}
+
+
 void test_Avance_Valon()
 {
     //Permet au robot d'avancer en ligne droite (simple) durant 1s
@@ -75,9 +122,10 @@ int main() {
         do
         {
             cout <<"Pour démarrer le robot merci de rentrer le scénario choisi : " << endl << "\t" 
-                << "0 pour stop le programme"<< endl << "\t" 
-                << "1 pour le test_Avance_Valon"<<endl
-                << "2 pour le test_Avance_cap_temps (5s) direction 90"<<endl;
+                << "0 pour stop le programme"<< endl << "\t"
+                << "1 pour les test_correcton_cap"<< endl << "\t"
+                << "2 pour le test_Avance_cap_temps (5s) direction 90"<<endl<< "\t"
+                << "3 pour le test_Avance_Valon" <<endl;
             cin>> debuter;        
             
             //selectionneur de scénario (mode)
@@ -87,10 +135,13 @@ int main() {
                     exit = false;
                     break;
                 case 1 :
-                    test_Avance_Valon();
+                    test_correcton_cap();
                     break;
                 case 2:
                     test_Avance_cap_temps();
+                    break;
+                case 3 :
+                    test_Avance_Valon();
                     break;
                 default:
                     break;
