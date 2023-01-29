845eb05362cb9a691c2cbff579870aeab72c89c6
Raphael-Nyphalem <Nyphalem@pm.me>
2023-01-29 16:03:17 +0100

    testRobot

--- jeu_de_test/testRobot.cpp
+++ jeu_de_test/testRobot.cpp
@@ -38,41 +38,13 @@ void test_avance_temps()
     cout<<" fin test avance Temps cap\n";
 }
 
-void test_tourne_droite()
+void test_tourne()
 {
     double tourneCap[MAX_PARAMETRE]={3,1,90,0,0};
     cout<<"debut test tourne cap (de 90° vest la droite)\n";
     execute_etape(tourneCap);
     cout<<" fin test avance tourne cap\n";
 }
-void test_execute()
-{
-    double stop[MAX_PARAMETRE]={0,1,0,0,0};
-    double avanceValon[MAX_PARAMETRE]= {1,1,-1,0,0};
-    double avanceTempsCap[MAX_PARAMETRE]= {2,1,10,-1,0};
-    double tourneCap[MAX_PARAMETRE]={3,1,90,0,0};
-
-    cout<<"debut test avance Valon\n";
-    execute_etape(avanceValon);
-    cout<<" fin test avance Valon\n";
-
-    cout<<"debut test avance Temps cap (cap du robot durant 10s)\n";
-    execute_etape(avanceTempsCap);
-    cout<<" fin test avance Temps cap\n";
-
-    cout<<"debut test tourne cap (de 90° vest la droite)\n";
-    execute_etape(tourneCap);
-    cout<<" fin test avance tourne cap\n";
-
-    cout<<"debut test stop\n";
-    avance_vit(100);
-    cout<<"on avance 2s avant de stop\n";
-    sleep_for(milliseconds(2000));
-    execute_etape(stop);
-    cout<<" fin test stop\n";
-    
-}
-
 
 //TEST
 void test_robot()
@@ -135,11 +107,10 @@ int main() {
         {
             cout <<"Pour démarrer le robot merci de rentrer le scénario choisi : " << endl 
                 << "\t0 pour stop le programme"<< endl 
-                << "\t1 pour les test de la fonction execute "<<endl
-                << "\t2 pour le test de la fonction robot"<<endl
-                << "\t3 pour le test de la fonction robot"<<endl
-                << "\t4 pour le test de la fonction robot"<<endl
-                << "\t5 pour le test de la fonction robot"<<endl;
+                << "\t1 pour les test de la fonction execute avance_Valon "<<endl
+                << "\t2 pour le test de la fonction execute avance_temps"<<endl
+                << "\t3 pour le test de la fonction execute tourne"<<endl
+                << "\t4 pour le test de la fonction robot"<<endl;
             cin>> debuter;        
             
             //selectionneur de scénario (mode)
@@ -148,20 +119,17 @@ int main() {
                 case 0:
                     exit = false;
                     break;
-                case 1 :
-                    test_execute();
-                    break;
-                case 2:
-                    test_robot();
-                    break;
-                case 3:
+                case 1:
                     test_avance_Valon();
                     break;
-                case 4:
+                case 2:
                     test_avance_temps();
                     break;
-                case 5:
-                    test_tourne_droite();
+                case 3:
+                    test_tourne();
+                    break;
+                case 4:
+                    test_robot();
                     break;
                 default:
                     break;
